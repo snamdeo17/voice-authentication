529640a8ac76bb2bdfc9cb073365a7ab68a9c08a
shinde_h <harsh.shinde7@gmail.com>
2021-03-08 16:33:18 +0530

    minor code changes

--- src/main/java/com/mastercard/authentication/repository/CustomerRepository.java
+++ src/main/java/com/mastercard/authentication/repository/CustomerRepository.java
@@ -1,9 +1,11 @@
 package com.mastercard.authentication.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mastercard.authentication.models.Customer;
 
+@Repository
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
 	public Customer findByEmail(String email);
 }
--- src/main/java/com/mastercard/authentication/repository/CustomerVoiceDataRepository.java
+++ src/main/java/com/mastercard/authentication/repository/CustomerVoiceDataRepository.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.mastercard.authentication.models.CustomerVoiceData;
 
-
+@Repository
 public interface CustomerVoiceDataRepository extends CrudRepository<CustomerVoiceData, Integer>{
 
 	 @Query("SELECT d FROM CustomerVoiceData d WHERE d.customer.userId=:userID")

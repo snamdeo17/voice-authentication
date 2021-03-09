dd3e99e28b497c1cdecc884ea0ec75a76f04c815
shinde_h <harsh.shinde7@gmail.com>
2021-03-09 13:43:17 +0530

    updating distance value

--- src/main/java/com/mastercard/authentication/services/AuthenticationService.java
+++ src/main/java/com/mastercard/authentication/services/AuthenticationService.java
@@ -112,7 +112,7 @@ public class AuthenticationService implements IAuthenticateService {
 						isMatched = true;
 						StringBuilder sb = new StringBuilder();
 
-						if (f.getDistance() < 0.4) {
+						if (f.getDistance() < 1.0) {
 							sb.append("Input sample matched!");
 							isMatched = isMatched && true;
 						} else {
